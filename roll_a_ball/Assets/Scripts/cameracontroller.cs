d654e8029874bb931f5931637cd1883db386f022
cth33115 <catherine.dowell94@gamil.com>
2020-08-22 21:14:46 -0500

    video 4

---
+++ roll_a_ball/Assets/Scripts/cameracontroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start () {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate () {
+        transform.position = player.transform.position + offset;
+    }
+}
